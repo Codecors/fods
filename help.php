2cc0b4536c6ef540b3a63eb847db351c68df785c
Codecors <info@stylehairs.tk>
2016-07-14 08:06:27 -0700

    Add files via upload

---
+++ help.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Indorigram | Search For food in indore</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="css/style.css">
+  <meta name="theme-color" content="#2C3E50" />
+  <meta name="mobile-web-app-capable" content="yes">
+  <link rel="manifest" href="manifest.json">
+  <meta name="mobile-web-app-capable" content="yes">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<style type="text/css">
+  #s1,#s2{display:none;margin-right:10px;float:left}#bwsprt,#s1,#s2{float:left}#overlays,.ifo{height:100%;width:100%}#overlays,.ifo,.igs1{width:100%}.container:active .circle,.container:active .circle1,.container:active .circle2,.container:active .circle3,.container:active .circle4,.container:active .circle5,.container:active .slide{transform:translateY(100px)}body{background:#fff!important}#overlays{position:fixed;background:#fff;z-index:1000;margin-top:-70px!important}.description{display:none}#s1{color:red}#s2{color:green}.ifo{border-top:1px solid rgba(0,0,0,.2);margin-top:5px;padding-top:5px}.igs1,.igs2{padding-top:50px}.igs1{display:block}.igs2{width:50%;display:none;margin-left:25%;height:500px}@media only screen and (min-width:330px){.igs1{display:none}.igs2{display:block}}
+</style>
+
+<body onload="dsp()">
+  <script type="text/javascript">
+    function dsp() {
+    document.getElementById('footer').style="display:none;";
+    }
+  </script>
+
+  <div class="main">
+     <div class="titlebar" style="background: #FFEC40;">
+    <span class="btn-left" onclick="goBack()"> &larr;</span>
+    <div class="header" style="font-variant: small-caps;font-weight: 800;">IndoriGram</div>
+    <a href="tel:+15555555555">
+      <div class="btn-right"><i class="material-icons">call</i></div>
+    </a>
+
+  </div>
+    <footer id="footer" style="height:auto;z-index:1000;">
+      <div id="status">Loading Please don't close. Website is loading...</div>
+      <span id="ldclse" onclick="document.getElementById('footer').style='display:none;';">+</span>
+    </footer>
+    <img class="igs1" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/48935/responsive-email.jpg">
+    <img class="igs2" src=" https://s32.postimg.org/q0xzwiz9h/image.jpg">
+    <div style="padding: 20px;padding-top: 70px;">
+      
+      <b>Your Browser is:</b> <font id="bwsr"> Detecting your browser</font>
+      <div class="ifo"><i id="s1" style="display:block;color:blue;" class="material-icons">info_outline</i><font id="bwsppt"></font></div>
+      <br><br>
+      <b>Your Browser Version: </b> <font id="vsr"></font>
+      <div class="ifo">
+        <i id="s1" class="material-icons">info_outline</i>
+        <i id="s2" class="material-icons">done_all</i>
+        <font id="bwsprt"></font>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+  <script type="text/javascript">
+    
+    var bsrsppt,bsrupg,opt=1,nVer=navigator.appVersion,nAgt=navigator.userAgent,browserName=navigator.appName,fullVersion=""+parseFloat(navigator.appVersion),majorVersion=parseInt(navigator.appVersion,10),nameOffset,verOffset,ix;-1!=(verOffset=nAgt.indexOf("OPR/"))?(browserName="Opera",fullVersion=nAgt.substring(verOffset+4),bsrsppt="Supported",Math.round(fullVersion)<38&&(bsrupg="Plese Update Your Browser",opt=0)):-1!=(verOffset=nAgt.indexOf("Opera"))?(browserName="Opera",fullVersion=nAgt.substring(verOffset+6),-1!=(verOffset=nAgt.indexOf("Version"))&&(fullVersion=nAgt.substring(verOffset+8)),bsrsppt="Not Supported",bsrupg="Plese Download Opera(not mini) or Chrome",opt=0):-1!=(verOffset=nAgt.indexOf("MSIE"))?(browserName="Microsoft Internet Explorer",fullVersion=nAgt.substring(verOffset+5),bsrsppt="Not Supported",bsrupg="Plese Download Chrome!",opt=0):-1!=(verOffset=nAgt.indexOf("Chrome"))?(browserName="Chrome",fullVersion=nAgt.substring(verOffset+7),bsrsppt="Supported",Math.round(fullVersion)<45&&(bsrupg="Plese Update Your Browser",opt=0)):-1!=(verOffset=nAgt.indexOf("Safari"))?(browserName="Safari",fullVersion=nAgt.substring(verOffset+7),-1!=(verOffset=nAgt.indexOf("Version"))&&(fullVersion=nAgt.substring(verOffset+8)),bsrsppt="Not Supported",Math.round(fullVersion)>38&&(bsrupg="Just Wait a bit! technology not in use for IOS",opt=0)):-1!=(verOffset=nAgt.indexOf("Firefox"))?(browserName="Firefox",fullVersion=nAgt.substring(verOffset+8),bsrsppt="Supported",Math.round(fullVersion)<46&&(bsrupg="Plese Update Your Browser",opt=0)):(nameOffset=nAgt.lastIndexOf(" ")+1)<(verOffset=nAgt.lastIndexOf("/"))&&(browserName=nAgt.substring(nameOffset,verOffset),fullVersion=nAgt.substring(verOffset+1),browserName.toLowerCase()==browserName.toUpperCase()&&(browserName=navigator.appName)),-1!=(ix=fullVersion.indexOf(";"))&&(fullVersion=fullVersion.substring(0,ix)),-1!=(ix=fullVersion.indexOf(" "))&&(fullVersion=fullVersion.substring(0,ix)),majorVersion=parseInt(""+fullVersion,10),isNaN(majorVersion)&&(fullVersion=""+parseFloat(navigator.appVersion),majorVersion=parseInt(navigator.appVersion,10)),0==opt?(document.getElementById("bwsprt").innerHTML=bsrupg,document.getElementById("s1").style="display:block;"):(document.getElementById("bwsprt").innerHTML="Your Browser is Updated! Carry On!",document.getElementById("s2").style="display:block;"),document.getElementById("bwsppt").innerHTML=bsrsppt,document.getElementById("bwsr").innerHTML=browserName,document.getElementById("vsr").innerHTML=majorVersion;
+  </script>
+      <!-- end of #container -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/list.js/1.0.0/list.min.js'></script>
+
+  <script type="text/javascript">
+    function goBack() {
+          window.history.back();
+      }
+  </script>
+
+
+</body>
+
+</html>
--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,23 @@
 var dataCacheName = "food",
   cacheName = "indorigram",
-  filesToCache = ["/", "index.php", "index.js", "style.css", "map.php", "css/style.css", "map.js", "https://fonts.googleapis.com/icon?family=Material+Icons", "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js", "https://cdnjs.cloudflare.com/ajax/libs/list.js/1.0.0/list.min.js"];
+  filesToCache = ["/",
+   "index.php",
+   "index.js",
+   "style.css",
+   "https://s32.postimg.org/t3q77cnnp/image.jpg",
+   "https://s31.postimg.org/dj42l7jjv/image.jpg",
+   "https://s32.postimg.org/e059qu6xh/image.jpg",
+   "https://s32.postimg.org/clpyhtn45/image.jpg",
+   "https://s32.postimg.org/5ad8my3fp/6_compressed_1.png",
+   "https://s31.postimg.org/aw1hwcuor/untitled.jpg",
+   "map.php",
+   "css/style.css",
+   "map.js",
+   "https://fonts.googleapis.com/icon?family=Material+Icons",
+   "https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js",
+   "https://cdnjs.cloudflare.com/ajax/libs/list.js/1.0.0/list.min.js"
+   ];
+
 self.addEventListener("install", function(e) {
 	
   console.log("[ServiceWorker] Install"), e.waitUntil(caches.open(cacheName).then(function(e) {
